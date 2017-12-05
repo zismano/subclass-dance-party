f2fefc9922eed52807d34788511a49adf0424c6b
zismano <zismanofir@gmail.com>
2017-12-04 22:32:09 -0800

    Add Batman class

---
+++ src/batmanDancer.js
@@ -0,0 +1,23 @@
+var BatmanDancer = function(top, left, timeBetweenSteps) { 
+ // this.$node = $('<img id="batman" src=src/batman.png /></img>'); 
+ // this.setPosition(top, left);
+ // this.$node.css({top: top, left: left});
+  Dancer.apply(this, arguments);
+  this.step();
+};
+
+BatmanDancer.prototype = Object.create(Dancer.prototype);
+
+BatmanDancer.prototype.constructor = BatmanDancer;
+
+// the basic dancer doesn't do anything interesting at all on each step,
+// it just schedules the next step
+BatmanDancer.prototype.step = function() { 
+  var name = Dancer.prototype.step.bind(this);
+  name();
+  // call the old version of step at the beginning of any call to this new version of step
+  this.$node.toggle();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+};

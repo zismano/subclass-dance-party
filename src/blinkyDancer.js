af6b4b3c22a01ee8664ecb51db0c424801223325
zismano <zismanofir@gmail.com>
2017-12-04 18:51:42 -0800

    Implement in pseudoclassical

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,7 +1,6 @@
 var BlinkyDancer = function(top, left, timeBetweenSteps) { 
-  this.oldStep = Dancer.prototype.step; 
- // this.oldStep();
   Dancer.apply(this, arguments);
+  this.step();
 
 };
 
@@ -9,35 +8,14 @@ BlinkyDancer.prototype = Object.create(Dancer.prototype);
 
 BlinkyDancer.prototype.constructor = BlinkyDancer;
 
-BlinkyDancer.prototype.step = function() {
-  this.oldStep();
-  // this.$oldNode.toggle();
+// the basic dancer doesn't do anything interesting at all on each step,
+// it just schedules the next step
+BlinkyDancer.prototype.step = function() { 
+  var name = Dancer.prototype.step.bind(this);
+  name();
+  // call the old version of step at the beginning of any call to this new version of step
   this.$node.toggle();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
 };
-
-
-
-// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
-//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-//   // so we must keep a copy of the old version of this function
-
-//   var oldStep = blinkyDancer.step;
-//   dancer.step = function() {
-//     // the basic dancer doesn't do anything interesting at all on each step,
-//     // it just schedules the next step
-//     setTimeout(dancer.step, timeBetweenSteps);
-//   };
-
-//   blinkyDancer.step = function() {
-//     // call the old version of step at the beginning of any call to this new version of step
-//     oldStep();
-//     // toggle() is a jQuery method to show/hide the <span> tag.
-//     // See http://api.jquery.com/category/effects/ for this and
-//     // other effects you can use on a jQuery-wrapped html tag.
-//     blinkyDancer.$node.toggle();
-//   };
-
-//   return blinkyDancer;
-// };

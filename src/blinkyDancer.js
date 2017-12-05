d444bf03fe5f164b64bc1e80f6c17ab8bb8708e1
zismano <zismanofir@gmail.com>
2017-12-04 16:15:09 -0800

    Refactor the code to pseudoclassical code, implement placing dancer nodes

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,40 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
-  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-  // so we must keep a copy of the old version of this function
-
-  var oldStep = blinkyDancer.step;
-
-  blinkyDancer.step = function() {
-    // call the old version of step at the beginning of any call to this new version of step
-    oldStep();
-    // toggle() is a jQuery method to show/hide the <span> tag.
-    // See http://api.jquery.com/category/effects/ for this and
-    // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
-
-  return blinkyDancer;
+var BlinkyDancer = function(top, left, timeBetweenSteps) { 
+  Dancer.apply(this, arguments);  
+  // this.oldStep = this.step;  
+  // this.oldStep();
 };
+
+BlinkyDancer.prototype = Object.create(Dancer.prototype);
+
+BlinkyDancer.prototype.constructor = BlinkyDancer;
+
+BlinkyDancer.prototype.step = function() {
+  this.oldStep();
+  this.$node.toggle();
+};
+
+
+
+// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+
+//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+//   // so we must keep a copy of the old version of this function
+
+//   var oldStep = blinkyDancer.step;
+//   dancer.step = function() {
+//     // the basic dancer doesn't do anything interesting at all on each step,
+//     // it just schedules the next step
+//     setTimeout(dancer.step, timeBetweenSteps);
+//   };
+//   blinkyDancer.step = function() {
+//     // call the old version of step at the beginning of any call to this new version of step
+//     oldStep();
+//     // toggle() is a jQuery method to show/hide the <span> tag.
+//     // See http://api.jquery.com/category/effects/ for this and
+//     // other effects you can use on a jQuery-wrapped html tag.
+//     blinkyDancer.$node.toggle();
+//   };
+
+//   return blinkyDancer;
+// };
